0ceb55a016a870277c7f07627c5aa0b5875ee5f7
Larry Bordowitz <laurence.bordowitz@yahoo.com>
2017-11-04 13:43:12 -0700

    add handlers, move auth to a separate file

---
+++ auth.go
@@ -0,0 +1,67 @@
+package main
+
+import (
+	"bufio"
+	"fmt"
+	"log"
+	"net/http"
+	"os"
+	"strings"
+	"syscall"
+
+	"golang.org/x/crypto/bcrypt"
+	"golang.org/x/crypto/ssh/terminal"
+)
+
+const hashNum = 6
+
+func addUser() {
+	username, password := credentials()
+	passHash, err := bcrypt.GenerateFromPassword(password, hashNum)
+	fmt.Print("Enter Password Again: ")
+	bytePassword, err := terminal.ReadPassword(int(syscall.Stdin))
+	fmt.Println()
+	if err != nil {
+		log.Fatal("Could not get the password, oh no!")
+	}
+	err = bcrypt.CompareHashAndPassword(passHash, bytePassword)
+	if err != nil {
+		log.Fatal("Passwords did not match, or some other thing went wrong")
+	}
+	// TODO
+	fmt.Println("This would be the part where we add username and passHash to the database: " + username)
+}
+
+func authUser(username string, password string, r *http.Request) bool {
+	if password == "" {
+		// TODO log
+		return false
+	}
+	// TODO
+	fmt.Println("This would be the part where we fetch the passHash from the database using this username: " + username)
+	passHash := []byte("wowo")
+	err := bcrypt.CompareHashAndPassword(passHash, []byte(password))
+	if err != nil {
+		// TODO log bad password
+		return false
+	}
+	return true
+}
+
+// credentials gets username and password credentials from the command line.
+// This was totally stolen from https://play.golang.org/p/l-9IP1mrhA
+func credentials() (string, []byte) {
+	reader := bufio.NewReader(os.Stdin)
+
+	fmt.Print("Enter Username: ")
+	username, _ := reader.ReadString('\n')
+
+	fmt.Print("Enter Password: ")
+	bytePassword, err := terminal.ReadPassword(int(syscall.Stdin))
+	fmt.Println()
+	if err != nil {
+		log.Fatal("Could not get the password, oh no!")
+	}
+
+	return strings.TrimSpace(username), bytePassword
+}
--- domestic.go
+++ domestic.go
@@ -1,28 +1,24 @@
 package main
 
 import (
-	"bufio"
 	"database/sql"
 	"encoding/json"
-	"fmt"
 	"io/ioutil"
 	"log"
 	"net/http"
 	"os"
-	"strings"
-	"syscall"
 
 	_ "github.com/go-sql-driver/mysql"
 
+	"github.com/gorilla/handlers"
 	"github.com/gorilla/mux"
+
+	"github.com/goji/httpauth"
 
 	"github.com/domestic-apps/domestic-api/chores"
 	"github.com/domestic-apps/domestic-api/tasks"
 
 	"gopkg.in/robfig/cron.v2"
-
-	"golang.org/x/crypto/bcrypt"
-	"golang.org/x/crypto/ssh/terminal"
 )
 
 type secrets struct {
@@ -38,40 +34,6 @@ func main() {
 	} else {
 		runServer()
 	}
-}
-
-func addUser() {
-	username, password := credentials()
-	passHash, err := bcrypt.GenerateFromPassword(password, 6)
-	fmt.Print("Enter Password Again: ")
-	bytePassword, err := terminal.ReadPassword(int(syscall.Stdin))
-	fmt.Println()
-	if err != nil {
-		log.Fatal("Could not get the password, oh no!")
-	}
-	err = bcrypt.CompareHashAndPassword(passHash, bytePassword)
-	if err != nil {
-		log.Fatal("Passwords did not match, or some other thing went wrong")
-	}
-	fmt.Println("This would be the part where we add username and passHash to the database: " + username)
-}
-
-// credentials gets username and password credentials from the command line.
-// This was totally stolen from https://play.golang.org/p/l-9IP1mrhA
-func credentials() (string, []byte) {
-	reader := bufio.NewReader(os.Stdin)
-
-	fmt.Print("Enter Username: ")
-	username, _ := reader.ReadString('\n')
-
-	fmt.Print("Enter Password: ")
-	bytePassword, err := terminal.ReadPassword(int(syscall.Stdin))
-	fmt.Println()
-	if err != nil {
-		log.Fatal("Could not get the password, oh no!")
-	}
-
-	return strings.TrimSpace(username), bytePassword
 }
 
 func runServer() {
@@ -109,5 +71,13 @@ func runServer() {
 	c := cron.New()
 	c.AddFunc("TZ="+timezone+" 0 0 7,19 * * *", tasksHandler.Cron) // 7am and 7pm every day
 	c.Start()
-	log.Fatal(http.ListenAndServeTLS(":443", s.Cert, s.Key, r))
+
+	// Add middleware (auth + logging)
+	o := httpauth.AuthOptions{
+		AuthFunc: authUser,
+	}
+	h := httpauth.BasicAuth(o)(r)
+	h = handlers.LoggingHandler(os.Stdout, h)
+
+	log.Fatal(http.ListenAndServeTLS(":443", s.Cert, s.Key, h))
 }
